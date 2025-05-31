46fa0a4783a9fda7f8bd33250babb61ae7bbe702
Jason Liu <jliu50@stuy.edu>
2025-05-31 00:14:24 -0400

    added args

--- phaseEncoding.py
+++ phaseEncoding.py
@@ -5,21 +5,34 @@ import argparse
 import sys
 
 mode = "encode"
+original = "clean.wav"
+output = "output.wav"
+message = "test"
+numberBytes = 0
 
 # Create the parser with 1 args
 parser = argparse.ArgumentParser(description="Parse up to 1 optional string arguments")
 parser.add_argument('str1', type=str, nargs='?', help='Mode')
+parser.add_argument('str2', type=str, nargs='?', help='Original Audio')
+parser.add_argument('str3', type=str, nargs='?', help='Output Audio')
+parser.add_argument('str4', type=str, nargs='?', help='Message/NumberBytes')
 
 args = parser.parse_args()
 
 args_dict = vars(args)
 provided_count = sum(1 for v in args_dict.values() if v is not None)
 
-if provided_count < 1:
-    print(f"Error: at least 5 arguments required, but only {provided_count} provided.\nCommand should provide args for the following in order, Image Name, Audio Name, Output Name, Image Height, Image Length, Image start X, Image start Y, Dark/Light Priority Mode, Visibility Percentage, Time Resolution of Image Multiplier, Frequency Resolution of Image Multiplier")
+if provided_count < 4:
+    print(f"Error: at least 4 arguments required, but only {provided_count} provided.\nCommand should provide args for the following in order, encode/decode Original Audio, Output Audio, message/number bytes")
     sys.exit(1)
 
 mode = args.str1
+original = args.str2
+output = args.str3
+if mode == "encode":
+    message = args.str4
+else:
+    numberBytes = int(args.str4)
 
 # STFT parameters
 n_fft = 2048
@@ -28,13 +41,13 @@ win_length = n_fft
 window = 'hann'
 
 if mode == "encode":
-    string = " \x00\x7f\x80\xff"
+    string = message
     bits = [int(bit) for char in string for bit in format(ord(char), '08b')]
 
-    print(bits)
+    # print(bits)
     print(len(bits))
 
-    y, sr = librosa.load("clean.wav", sr=None)
+    y, sr = librosa.load(original, sr=None)
 
     D_orig = librosa.stft(y, n_fft=n_fft, hop_length=hop_length, win_length=win_length, window=window)
     magnitude_orig, phase_orig = np.abs(D_orig), np.angle(D_orig)
@@ -53,14 +66,14 @@ if mode == "encode":
     D_mod = magnitude_orig * np.exp(1j * phase_mod)
     y_mod = librosa.istft(D_mod, hop_length=hop_length, win_length=win_length, window=window)
 
-    sf.write("output.wav", y_mod, sr)
+    sf.write(output, y_mod, sr)
 
 if mode == "decode":
-    y, sr = librosa.load("clean.wav", sr=None)
+    y, sr = librosa.load(original, sr=None)
     D_orig = librosa.stft(y, n_fft=n_fft, hop_length=hop_length, win_length=win_length, window=window)
     _, phase_orig = np.abs(D_orig), np.angle(D_orig)
 
-    y_reloaded, sr = librosa.load("output.wav", sr=None)
+    y_reloaded, sr = librosa.load(output, sr=None)
     D_reloaded = librosa.stft(y_reloaded, n_fft=n_fft, hop_length=hop_length, win_length=win_length, window=window)
     _, phase_reloaded = np.abs(D_reloaded), np.angle(D_reloaded)
 
@@ -70,7 +83,7 @@ if mode == "decode":
     total_time_buckets = phase_orig.shape[1]
     total_bits = total_time_buckets // buckets_per_bit
 
-    for i in range(total_bits):
+    for i in range(numberBytes):
         start = i * buckets_per_bit
         end = start + buckets_per_bit
         if end > total_time_buckets:
@@ -79,10 +92,10 @@ if mode == "decode":
         mean_diff = np.mean(np.abs(diff))
         if mean_diff > 1.4:
             reconstructedBits.append(1)
-            print(f"Bit {i}: Mean phase difference = {mean_diff:.3f}")
+            # print(f"Bit {i}: Mean phase difference = {mean_diff:.3f}")
         else:
             reconstructedBits.append(0)
-            print(f"Bit {i}: Mean phase difference = {mean_diff:.3f}")
+            # print(f"Bit {i}: Mean phase difference = {mean_diff:.3f}")
 
     print(reconstructedBits)
     string = ""
@@ -91,7 +104,7 @@ if mode == "decode":
         if len(byte) < 8:
             break
         ascii_val = int("".join(map(str, byte)), 2)
-        print(chr(ascii_val), ascii_val)
+        # print(chr(ascii_val), ascii_val)
         string += chr(ascii_val)
 
     print(string)
